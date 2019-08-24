a3b3842be3e00573b5c13187a3e2a0f2d1416904
da0ke <592052922@qq.com>
2019-08-24 11:05:24 +0800

    1.修复-NumberUtils.isInteger:缺少判断数字超出最大值的情况;2.废弃-NumberUtils:isInt替代isInteger

--- src/main/java/cn/da0ke/javakit/Test.java
+++ src/main/java/cn/da0ke/javakit/Test.java
@@ -1,20 +1,9 @@
 package cn.da0ke.javakit;
 
-import java.text.ParseException;
-import java.util.Date;
-
-import cn.da0ke.javakit.utils.TimeUtils;
-
 public class Test {
-
-//	public static void main(String[] args) throws ParseException {
-//		Test test = new Test();
-//		test.test();
-//	}
-//
-//	private void test() throws ParseException {
-//
-//	}
-
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
--- src/main/java/cn/da0ke/javakit/utils/NumberUtils.java
+++ src/main/java/cn/da0ke/javakit/utils/NumberUtils.java
@@ -1,24 +1,61 @@
 package cn.da0ke.javakit.utils;
 
 public class NumberUtils {
-
-	public static boolean isInteger(String s) {
-		String rule = "^-?[1-9]\\d*$";
-		return s!=null && s.matches(rule);
+	
+	/**
+	 * 是否为Integer
+	 * @param s
+	 * @return
+	 */
+	@Deprecated
+	public static boolean isInteger(String str) {
+		if (str == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (final NumberFormatException nfe) {
+            return false;
+        }
 	}
 	
-	public static int string2int(String s) {
-		return string2int(s, 0);
+	/**
+	 * 是否为int
+	 * @param str 字符串
+	 * @return boolean
+	 */
+	public static boolean isInt(String str) {
+		if (str == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (final NumberFormatException nfe) {
+            return false;
+        }
 	}
 	
-	public static int string2int(String s, int def) {
-		int rs;
-		try {
-			rs = Integer.parseInt(s);
-		} catch(NumberFormatException e) {
-			rs = def;
-		}
-		return rs;
+	/**
+	 * 字符串转换为int
+	 * @param str 字符串
+	 * @param defaultValue 默认值
+	 * @return int
+	 */
+	public static int string2int(String str, int defaultValue) {
+		if (str == null) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(str);
+        } catch (final NumberFormatException nfe) {
+            return defaultValue;
+        }
+	}
+	
+	public static int string2int(String str) {
+		return string2int(str, 0);
 	}
 	
 }
